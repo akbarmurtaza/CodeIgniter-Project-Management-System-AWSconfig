52a59f3b5996475c9e2245f0fa5ad5cf3e85b843
unknown <murtaza.bhai13@gmail.com>
2018-11-24 23:49:14 +0500

    Updated Basepath with AWS config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://management.nerdwaretech.com/cms/';
+$config['base_url'] = 'http://34.207.179.83/CodeIgniter-Project-Management-System-AWSconfig/';
 
 /*
 |--------------------------------------------------------------------------
